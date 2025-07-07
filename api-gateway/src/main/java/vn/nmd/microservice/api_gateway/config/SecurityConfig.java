cacff9f70cb7c2b174d8d1408d4964b358e6aef3
Nguyen Minh Dang <minhdangb3@gmail.com>
2025-07-07 15:32:22 +0700

    [DangNM] aggregate all swager into gateway

--- api-gateway/src/main/java/vn/nmd/microservice/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/vn/nmd/microservice/api_gateway/config/SecurityConfig.java
@@ -9,9 +9,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+	// Allow to view swagger-ui and api-docs without authentication
+	private final String[] FREE_RESOURCE_URL = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+			"/swagger-resources/**", "/aggregate/**", "/api-docs/**",};
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+		return httpSecurity.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers(FREE_RESOURCE_URL).permitAll()
+				.anyRequest().authenticated())
 				.oauth2ResourceServer(oauthe2 -> oauthe2.jwt(Customizer.withDefaults())).build();
 	}
 }
--- api-gateway/src/main/java/vn/nmd/microservice/api_gateway/route/Routes.java
+++ api-gateway/src/main/java/vn/nmd/microservice/api_gateway/route/Routes.java
@@ -1,6 +1,7 @@
 package vn.nmd.microservice.api_gateway.route;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -20,30 +21,59 @@ public class Routes {
 
 	@Value("${inventory.service.url}")
 	private String inventoryServiceUrl;
-	
+
 	@Bean
-	public RouterFunction<ServerResponse> productServiceRoute(){
-		
+	public RouterFunction<ServerResponse> productServiceRoute() {
+
 		// This configuration below mean:
 		// Router Id: product_service
-		// Every request url that match '/api/product' will be forwarded to productServiceUrl
-		// Note: You can use another function that RequestPredicates support to match your request with the destination.
+		// Every request url that match '/api/product' will be forwarded to
+		// productServiceUrl
+		// Note: You can use another function that RequestPredicates support to match
+		// your request with the destination.
 		return GatewayRouterFunctions.route("product_service")
-				.route(RequestPredicates.path("/api/product"), HandlerFunctions.http(productServiceUrl))
-				.build();
+				.route(RequestPredicates.path("/api/product"), HandlerFunctions.http(productServiceUrl)).build();
 	}
-	
+
 	@Bean
-	public RouterFunction<ServerResponse> orderServiceRoute(){
+	public RouterFunction<ServerResponse> orderServiceRoute() {
 		return GatewayRouterFunctions.route("order_service")
-				.route(RequestPredicates.path("/api/order"), HandlerFunctions.http(orderServiceUrl))
+				.route(RequestPredicates.path("/api/order"), HandlerFunctions.http(orderServiceUrl)).build();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> inventoryServiceRoute() {
+		return GatewayRouterFunctions.route("inventory_service")
+				.route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http(inventoryServiceUrl)).build();
+	}
+	
+	// Aggregate routes for swagger-ui and api-docs
+
+	@Bean
+	public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+		return GatewayRouterFunctions.route("inventory_service_swagger")
+				.route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"),
+						HandlerFunctions.http(inventoryServiceUrl))
+				.before(BeforeFilterFunctions.rewritePath("/aggregate/inventory-service/v3/api-docs", "/api-docs"))
+				.build();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+		return GatewayRouterFunctions.route("order_service_swagger")
+				.route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"),
+						HandlerFunctions.http(orderServiceUrl))
+				.before(BeforeFilterFunctions.rewritePath("/aggregate/order-service/v3/api-docs", "/api-docs")).build();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+		return GatewayRouterFunctions.route("product_service_swagger")
+				.route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"),
+						HandlerFunctions.http(productServiceUrl))
+				.before(BeforeFilterFunctions.rewritePath("/aggregate/product-service/v3/api-docs", "/api-docs"))
 				.build();
 	}
 	
-	@Bean
-	public RouterFunction<ServerResponse> inventoryServiceRoute(){
-		return GatewayRouterFunctions.route("inventory_service")
-				.route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http(inventoryServiceUrl))
-				.build();
-	}
+	// End Aggregate routes for swagger-ui and api-docs
 }
